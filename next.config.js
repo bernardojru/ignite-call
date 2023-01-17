6c2b06d58a78eb49c3bfa712fbffa098d8ae3f05
bernardojru <bernardogomes860@gmail.com>
2023-01-17 04:46:32 +0100

    advance: start project and install all dependences

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  pageExtensions: ["page.tsx", "api.ts", "api.tsx"], // considera que são páginas e rotas.
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
